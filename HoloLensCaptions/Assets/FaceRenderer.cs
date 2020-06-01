24bbd78fae3603bc61c5f0f16e215101e34f1d53
Johnson Kuang <johnsonkuang@outlook.com>
2020-06-01 11:43:48 -0700

    asf

---
+++ HoloLensCaptions/Assets/FaceRenderer.cs
@@ -0,0 +1,281 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+#if WINDOWS_UWP
+using FaceProcessing;
+#endif
+
+
+public class FaceRenderer : MonoBehaviour
+{
+    [Tooltip("Distance between the pupils of the subject's face, 63mm is the human average.")]
+    public float InterPupilDistance = 0.063f;   
+    [Tooltip("Number of landmarks returned by the face tracker.")]
+    public int NumberOfLandmarks = 51;
+
+    public Mesh NeutralFace;
+    public Mesh[] Blendshapes;
+
+    List<GameObject> cubes;
+    Vector3[] landmarkPositions;
+    bool modelFitterInitialized = false;
+    Matrix4x4 K;
+    HololensCameraUWP webcam;
+    float[] blendshapeWeights = new float[0];
+    float lastUpdateTimestamp;
+
+    bool bShowDebug = false;
+
+    float meshScale;
+#if WINDOWS_UWP
+    ModelFitter modelFitter;
+    PositionFilter xFilter, yFilter, zFilter;
+#endif
+
+    //indices of the 2D landmarks and corresponding indices of vertices in the candide face model
+    int[] idxs2D = { 18, 14, 13, 16, 11, 17,
+                    15, 19, 20, 21, 22, 23,
+                    24, 25, 26, 27, 28, 29,
+                    30,  0,  2,  4,  5,  7,
+                    9, 31, 34, 37, 40, 36,
+                    32, 38, 42, 43, 45, 47,
+                    49 };
+    int[] idxs3D = { 25, 58,  4,  5, 93, 110,
+                    111, 52, 97, 103, 55,
+                    109, 99, 22, 102,  96,
+                    19,  98, 108,  47,  48,
+                    49,  16,  15,  14,  63,
+                    6,  30,   7,  78,  79,
+                    84,  85,  88,  86,  87,
+                    39 };
+
+
+    public float[] BlendshapeWeights
+    {
+        get
+        {
+            return blendshapeWeights;
+        }
+    }
+
+    void Start()
+    {
+
+        meshScale = GetMeshScale();
+   
+        cubes = new List<GameObject>();
+        for (int i = 0; i < NumberOfLandmarks; i++)
+        {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.localScale = new Vector3(0.005f, 0.005f, 0.005f);
+
+            cubes.Add(cube);
+        }
+
+        landmarkPositions = new Vector3[NumberOfLandmarks];
+        HideDebug();
+    }
+
+    void Update()
+    {
+        if (webcam.Initialized && !modelFitterInitialized)
+        {
+            InitializeModelFitter();                    
+            modelFitterInitialized = true;
+        }
+    }
+
+    public void SetWebcam(HololensCameraUWP webcam)
+    {
+        this.webcam = webcam;
+    }
+
+    public void UpdateHeadPose(float[] landmarks, Matrix4x4 webcamToWorldTransform)
+    {
+
+        if (!modelFitterInitialized)
+            return;
+
+        float rotX = 0.0f, rotY = 0.0f, rotZ = 0.0f;
+        float tX = 0.0f, tY = 0.0f, tZ = 0.0f;
+
+#if WINDOWS_UWP
+        modelFitter.Fit3D(landmarks, out rotX, out rotY, out rotZ, out tX, out tY, out tZ, out blendshapeWeights, 2);
+#endif      
+        Vector3 modelPosition = new Vector3(tX, tY, tZ);
+        modelPosition = GetModelPosition(modelPosition, webcamToWorldTransform, K);   
+        
+        UpdateLandamarkPositions(landmarks, webcamToWorldTransform, (modelPosition - (Vector3)webcamToWorldTransform.GetColumn(3)).magnitude);
+        if (bShowDebug)
+        {
+            DrawLandmarks();
+        }
+    
+        lastUpdateTimestamp = Time.realtimeSinceStartup;
+    }
+
+    public void ResetFitter()
+    {
+#if WINDOWS_UWP
+        modelFitter.ResetPose();
+        xFilter.Reset();
+        yFilter.Reset();
+        zFilter.Reset();
+#endif     
+        lastUpdateTimestamp = -1.0f;
+    }
+
+    public float[] GetLandmarkProjections(Matrix4x4 webcamToWorldMatrix)
+    {
+        float[] landmarkProjections = new float[landmarkPositions.Length * 2];
+
+        Matrix4x4 worldToWebcamMatrix = webcamToWorldMatrix.inverse;
+        for (int i = 0; i < landmarkPositions.Length; i++)
+        {
+            Vector2 proj = ProjectVector(worldToWebcamMatrix.MultiplyPoint3x4(landmarkPositions[i]));
+            landmarkProjections[2 * i] = proj.x;
+            landmarkProjections[2 * i + 1] = proj.y;
+        }
+
+        return landmarkProjections;
+    }
+
+
+    private Vector3 GetModelPosition(Vector3 vertex, Matrix4x4 webcamToWorldTransform, Matrix4x4 K)
+    {
+        Vector3 projectedHomo = K * vertex;
+        Vector2 projected = new Vector3(projectedHomo.x / projectedHomo.z, projectedHomo.y / projectedHomo.z);
+
+        var webcamSpacePosition = vertex.magnitude * Pix2WebcamSpacePos(projected.x, projected.y).normalized;
+        var cameraPosition = webcamToWorldTransform * (new Vector4(0, 0, 0, 1));
+
+        return webcamToWorldTransform * webcamSpacePosition + cameraPosition;
+    }
+
+    float GetMeshScale()
+    {
+        //indices of the corners of the left eye in the Candide mesh
+        int lEyelCornerIdx = 52;
+        int lEyeRCornerIdx = 55;
+        //indices of the corners of the right eye in the Candide mesh
+        int rEyelCornerIdx = 19;
+        int rEyeRCornerIdx = 22;
+
+        Vector3 leftEyeCenter = (NeutralFace.vertices[lEyelCornerIdx] + NeutralFace.vertices[lEyeRCornerIdx]) / 2;
+        Vector3 rightEyeCenter = (NeutralFace.vertices[rEyelCornerIdx] + NeutralFace.vertices[rEyeRCornerIdx]) / 2;
+        float curDist = (leftEyeCenter - rightEyeCenter).magnitude;
+
+        return InterPupilDistance / curDist;
+    }
+
+    private Vector2 ProjectVector(Vector3 vec)
+    {
+        Vector4 vec4 = new Vector4(vec.x, vec.y, vec.z, 1);
+        Vector3 projectedHomo = K * vec4;
+        Vector2 projected = new Vector2(webcam.Width - projectedHomo.x / projectedHomo.z, projectedHomo.y / projectedHomo.z);
+
+        return projected;
+    }
+
+    private Vector3 Pix2WebcamSpacePos(float x, float y)
+    {
+        Vector3 res = new Vector3(0.0f, 0.0f, 0.0f);
+        res.z = 1.0f;
+        res.x = ((webcam.Width - x) - K.m02) / K.m00;
+        res.y = (y - K.m12) / K.m11;
+
+        return res * -1.0f;
+    }
+
+    //use this code to transform bounding box coordinates
+    private void UpdateLandamarkPositions(float[] landmarks, Matrix4x4 webcamToWorldTransform, float distToFace)
+    {
+        var WorldSpaceRayPoint1 = webcamToWorldTransform * (new Vector4(0, 0, 0, 1));
+
+        for (int i = 0; i < landmarks.Length / 2; i++)
+        {
+            float posX = landmarks[i * 2];
+            float posY = landmarks[i * 2 + 1];
+
+            var webcamSpacePos = Pix2WebcamSpacePos(posX, posY);
+            var WorldSpaceRayPoint2 = webcamToWorldTransform * webcamSpacePos;
+
+            landmarkPositions[i] = WorldSpaceRayPoint1 + distToFace * WorldSpaceRayPoint2.normalized;
+        }
+    }
+
+    private void DrawLandmarks()
+    {
+        for (int i = 0; i < landmarkPositions.Length; i++)
+        {
+            cubes[i].transform.position = landmarkPositions[i];
+        }
+    }
+
+    void InitializeModelFitter()
+    {
+        float[] floatMean3DShape = new float[NeutralFace.vertices.Length * 3];
+        for (int i = 0; i < NeutralFace.vertices.Length; i++)
+        {
+            floatMean3DShape[3 * i + 0] = meshScale * NeutralFace.vertices[i].x;
+            floatMean3DShape[3 * i + 1] = meshScale * NeutralFace.vertices[i].y;
+            floatMean3DShape[3 * i + 2] = meshScale * NeutralFace.vertices[i].z;
+        }
+
+        List<IList<float>> listBlendshapes = new List<IList<float>>();
+
+        for (int i = 0; i < Blendshapes.Length; i++)
+        {
+            float[] blendshape = new float[NeutralFace.vertices.Length * 3];
+            for (int j = 0; j < Blendshapes[i].vertices.Length; j++)
+            {
+                blendshape[3 * j + 0] = meshScale * (Blendshapes[i].vertices[j].x - NeutralFace.vertices[j].x);
+                blendshape[3 * j + 1] = meshScale * (Blendshapes[i].vertices[j].y - NeutralFace.vertices[j].y);
+                blendshape[3 * j + 2] = meshScale * (Blendshapes[i].vertices[j].z - NeutralFace.vertices[j].z);
+            }
+
+            listBlendshapes.Add(new List<float>(blendshape));
+        }
+
+
+#if WINDOWS_UWP
+        int w = webcam.Width;
+        int h = webcam.Height;
+        Matrix4x4 normK = webcam.ProjectionMatrix;
+        float[] arrayK = new float[9];
+        arrayK[0 * 3 + 0] = w * normK.m00 / 2;
+        arrayK[1 * 3 + 1] = h * normK.m11 / 2;
+        arrayK[0 * 3 + 2] = w * (normK.m02 + 1) / 2;
+        arrayK[1 * 3 + 2] = h * (normK.m12 + 1) / 2;
+        arrayK[2 * 3 + 2] = 1;
+        K = webcam.ProjectionMatrix;
+        K.m00 = w * normK.m00 / 2;
+        K.m11 = h * normK.m11 / 2;
+        K.m02 = w * (normK.m02 + 1) / 2;
+        K.m12 = h * (normK.m12 + 1) / 2;
+        K.m22 = 1;
+
+        modelFitter = new ModelFitter(floatMean3DShape, listBlendshapes, idxs2D, idxs3D, arrayK);
+#endif
+    }
+
+    public void ShowDebug()
+    {
+        for (int i = 0; i < cubes.Count; i++)
+        {
+            cubes[i].SetActive(true);
+        }
+        bShowDebug = true;
+    }
+
+    public void HideDebug()
+    {
+        for (int i = 0; i < cubes.Count; i++)
+        {
+            cubes[i].SetActive(false);
+        }
+        bShowDebug = false;
+    }
+}
---
+++ HoloLensCaptions/Assets/Localization.cs
@@ -0,0 +1,216 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class Localization : MonoBehaviour
+{
+    public static Localization instance;
+
+    public GameObject arrow; // arrow is used for 2D
+    public GameObject indicatorPrefeb; // indicator is used for 3D
+    public GameObject[] TwelveDir; // used for 12-dir display. Make sure to et size = 12 in the editor.
+    public float fadeDuration = 1; // how long does it take to let one indicator fade out.
+    public long packetCount = 0;
+    private Vector3 soundSourceLocaltion;
+
+    private Dictionary<GameObject, float> alphaTable = new Dictionary<GameObject, float>();
+    private const int thetaDivision = 12;
+    private const int phiDivision = 6;
+    private float[,] directionActivity = new float[thetaDivision, phiDivision];
+
+    private List<GameObject> indicators;
+
+    public void Awake()
+    {
+        instance = this;
+        indicators = new List<GameObject>();
+    }
+
+
+    public void setSoundSourceAngle(int angle)
+    {
+        angle = -angle;
+        Transform cam = Camera.main.transform;
+        soundSourceLocaltion = cam.position + Quaternion.AngleAxis(angle, cam.up) * cam.forward * 3;
+    }
+
+    public void Update()
+    {
+        clearIndicators();
+        // Check reach potentially active, and map then to one of 12 directions.
+        // Right now we only take the maximum.
+        for (int i = 0; i < directionActivity.GetLength(0); i++)
+        {
+            for (int j = 0; j < directionActivity.GetLength(1); j++)
+            {
+                float activity = directionActivity[i, j];
+
+                float x = Mathf.Sin(j * (Mathf.PI / phiDivision)) * Mathf.Cos(i * (2 * Mathf.PI / thetaDivision));
+                float y = Mathf.Sin(j * (Mathf.PI / phiDivision)) * Mathf.Sin(i * (2 * Mathf.PI / thetaDivision));
+                float z = Mathf.Cos(j * (Mathf.PI / phiDivision));
+
+                Vector3 dir = new Vector3(x, y, z);
+
+                mapSourceDirectionToIndicators(dir, activity);
+
+                directionActivity[i, j] = Mathf.Max(0, directionActivity[i, j] - Time.deltaTime / fadeDuration);
+            }
+        }
+
+    }
+
+    /*
+     * Used by other classes to update the visual of sound source indicators.
+     * 
+     */
+    public void updateLocalization2D()
+    {
+        Transform cam = Camera.main.transform;
+        Vector3 originToSoundSource = soundSourceLocaltion - cam.position;
+        float angle = Vector3.SignedAngle(cam.forward, originToSoundSource, cam.up);
+        Debug.DrawRay(cam.position, cam.forward, Color.red, 0.1f);
+        Debug.DrawRay(cam.position, originToSoundSource, Color.blue, 0.1f);
+        arrow.GetComponent<RectTransform>().localEulerAngles = new Vector3(0, 0, -angle);
+    }
+
+    /*
+    * Used by other classes to update the visual of sound source indicators.
+    * 
+    */
+    public void updateLocalization3D(string jsonMessage)
+    {
+        packetCount++;
+        print(packetCount / Time.time);
+        JObject o;
+        try
+        {
+            o = JObject.Parse(jsonMessage);
+        }
+        catch (JsonReaderException e)
+        {
+            return;
+        }
+
+        JArray points = (JArray)o["src"];
+        List<GameObject> temp = new List<GameObject>();
+        foreach (JObject obj in points.Children())
+        {
+            float x = float.Parse(obj["x"].ToString());
+            float y = float.Parse(obj["y"].ToString());
+            float z = float.Parse(obj["z"].ToString());
+            float activity = float.Parse(obj["activity"].ToString()); // activity is the probablity that the sound source exists
+            if (activity > 0.0f)
+            {
+
+                // we've found a source
+                Vector3 pos = Camera.main.transform.position + 10 * x * new Vector3(1, 0, 0)
+                    + 10 * y * new Vector3(0, 0, 1)
+                    + 10 * z * new Vector3(0, 1, 0);
+                GameObject go = Instantiate(indicatorPrefeb, pos, Quaternion.identity);
+                temp.Add(go);
+                //clearIndicators();
+            }
+        }
+        if (temp.Count > 0.5f)
+        {
+            indicators = temp;
+        }
+    }
+
+    /*
+    * Used by other classes to update the visual of sound source indicators.
+    * 
+    */
+    public void updateLocalization12Directions(string jsonMessage)
+    {
+        JObject o;
+        try
+        {
+            o = JObject.Parse(jsonMessage);
+        }
+        catch (JsonReaderException e)
+        {
+            return;
+        }
+
+        JArray points = (JArray)o["src"];
+        List<GameObject> temp = new List<GameObject>();
+
+        foreach (JObject obj in points.Children())
+        {
+            float x = float.Parse(obj["x"].ToString());
+            float y = float.Parse(obj["y"].ToString());
+            float z = float.Parse(obj["z"].ToString());
+            float activity = float.Parse(obj["activity"].ToString()); // activity is the probablity that the sound source exists
+            if (activity > 0.0f)
+            {
+                Vector2 xy = new Vector2(x, y);
+                float signedTheta = Vector2.SignedAngle(xy, new Vector2(1, 0)) / 180 * Mathf.PI; // ranges from -pi to pi
+                float unsignedTheta = signedTheta < 0 ? signedTheta + 2 * Mathf.PI : signedTheta; // ranges from 0 to 2pi
+
+                int tIndexFloored = Mathf.FloorToInt(unsignedTheta / (2 * Mathf.PI / thetaDivision));
+                //int tIndex = tIndexFloored + Mathf.RoundToInt((unsignedTheta - tIndexFloored * (2 * Mathf.PI / thetaDivision)) / (2 * Mathf.PI / thetaDivision));
+
+                Vector3 xyz = new Vector3(x, y, z);
+                float phi = Vector3.Angle(new Vector3(0, 0, 1), xyz) / 180 * Mathf.PI; // phi is always unsigned
+                // print(phi * 180 / Mathf.PI);
+                int pIndexFloored = Mathf.FloorToInt(phi / (Mathf.PI / phiDivision));
+                //int pIndex = pIndexFloored + Mathf.RoundToInt((phi - pIndexFloored * (Mathf.PI / pIndexFloored)) / (Mathf.PI / phiDivision));
+                pIndexFloored = 3;
+                directionActivity[tIndexFloored, pIndexFloored] = 1; // set activitiy on the direction to 1, fully active.
+            }
+        }
+    }
+
+    /*
+     * This is method work works with updateLocalization3D()
+     */
+    private void clearIndicators()
+    {
+        // set opacity to 0
+        foreach (GameObject go in TwelveDir)
+        {
+            Color c = go.GetComponent<SpriteRenderer>().color;
+            c.a = 0;
+            go.GetComponent<SpriteRenderer>().color = c;
+        }
+    }
+
+    /*
+     * updateLocalization12Directions() will map incoming sound source
+     * directions (which is continuous) to a set of discrete vectors
+     * spanning 360 degree. This function will then map each of the vectors
+     * to the (12) visual indicators.
+     * Note that activity ranges from 0 to 1.
+     */
+    private void mapSourceDirectionToIndicators(Vector3 dir, float activity)
+    {
+        float y = dir.y;
+        float z = dir.z;
+        dir.z = y;
+        dir.y = z;
+
+        Vector3 forward = Camera.main.transform.forward;
+        Vector3 up = Camera.main.transform.up;
+        /*
+        print("forward : " + Camera.main.transform.forward + " up: " + Camera.main.transform.up);
+
+        Vector3 forward = new Vector3(0, 1, 0);
+        Vector3 up = new Vector3(0, 0, 1);*/
+
+        int angle = Mathf.RoundToInt(Vector3.SignedAngle(dir, forward, up));
+
+        if (activity == 1)
+        {
+            print(angle);
+        }
+        int index = (int)(((angle + 360) % 360) / (360 / TwelveDir.Length));
+        TwelveDir[index].SetActive(true);
+
+        Color c = TwelveDir[index].GetComponent<SpriteRenderer>().color;
+        c.a = Mathf.Max(c.a, activity);
+        TwelveDir[index].GetComponent<SpriteRenderer>().color = c;
+    }
+}
